3da65dd6dce12d7ade760d1d7be438cb4e73ffbf
nmwei <254060001@qq.com>
2022-01-04 18:50:53 +0800

    初始化C语言命令行项目

---
+++ C/C/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  C
+//
+//  Created by 倪梦威 on 2022/1/4.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
